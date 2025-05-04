81b8517e40b6ce4e74dab09d965931aea1937869
anuragganji <anuragganji09@gmail.com>
2025-05-05 02:15:50 +0530

    Initial commit

---
+++ src/main/java/com/anurag/parallelapicalls/ParallelApiCallsApplication.java
@@ -0,0 +1,13 @@
+package com.anurag.parallelapicalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelApiCallsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParallelApiCallsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anurag/parallelapicalls/ParallelApiCallsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurag.parallelapicalls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelApiCallsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
